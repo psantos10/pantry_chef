10fed57ea7944957bf8e7bd73b7c4622994d6240
Patrício dos Santos <hello@psantos.dev>
2024-09-05 17:04:13 +0100

    Setup FactoryBot for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,16 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# Load support files
+Dir[Rails.root.join("test", "support", "**", "*.rb")].each { |f| require f }
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    # fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
