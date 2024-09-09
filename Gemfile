8fcc9510b344f681df43bee59aa932d4103c5d78
Patrício dos Santos <hello@psantos.dev>
2024-09-09 19:32:47 +0100

    Install hotwire-combobox for searcheable input

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem "hotwire_combobox"
 gem "kaminari"
 
 group :development, :test do
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,9 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+import HwComboboxController from "@josefarias/hotwire_combobox"
+application.register("hw-combobox", HwComboboxController)
 
 export { application }
