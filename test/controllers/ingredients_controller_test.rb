ed2dd47369b9a0d8edac3c66e9f4bb4da9c0a4d7
Patrício dos Santos <hello@psantos.dev>
2024-09-09 21:51:54 +0100

    Fix test

--- test/controllers/ingredients_controller_test.rb
+++ test/controllers/ingredients_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class IngredientsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    user = create(:user)
+    login_as(user)
+  end
+
   test "should get index" do
     get ingredients_index_url
     assert_response :success
