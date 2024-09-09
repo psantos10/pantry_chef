7a2f88ba3d107e20aa336096f71e341ea637e0cd
Patrício dos Santos <hello@psantos.dev>
2024-09-09 19:52:57 +0100

    Show recipe details

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,11 @@
 class RecipesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @recipes = Recipe.all.page(params[:page]).per(9)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 end
