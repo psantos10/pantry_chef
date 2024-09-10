0e9f894d50163789ca81d579081a256af835810e
Patrício dos Santos <hello@psantos.dev>
2024-09-10 09:07:35 +0100

    Test Job

--- test/jobs/import_recipes_job_test.rb
+++ test/jobs/import_recipes_job_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
+require "minitest/mock"
 
 class ImportRecipesJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @recipes_json = File.read(Rails.root.join("test", "fixtures", "files", "recipes.json"))
+    @parsed_recipes = JSON.parse(@recipes_json)
+  end
+
+  test "enqueues the job with recipes as arguments" do
+    assert_enqueued_with(job: ImportRecipesJob, args: [ recipes: @parsed_recipes ]) do
+      ImportRecipesJob.perform_later(recipes: @parsed_recipes)
+    end
+  end
+
+  test "calls RecipeImporterService and creates 3 recipes" do
+    ImportRecipesJob.perform_now(recipes: @parsed_recipes)
+
+    assert_equal 3, Recipe.count
+  end
 end
